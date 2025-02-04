70f2711c319ee371c6acc7c12f9efdbb2652f42c
Thomas Bombach <tbombach@users.noreply.github.com>
2025-02-04 00:44:04 -0800

    Update startServer.sh

--- .devcontainer/startServer.sh
+++ .devcontainer/startServer.sh
@@ -4,7 +4,7 @@ export JAVA8HOME=/usr/local/sdkman/candidates/java/8.0.382-tem
 export JAVA11HOME=/usr/local/sdkman/candidates/java/11.0.20-tem
 export JAVA17HOME=/usr/local/sdkman/candidates/java/17.0.8-tem
 export JAVA21HOME=/usr/local/sdkman/candidates/java/21.0.4-tem
-export JAVA_HOME=$JAVA11HOME
+export JAVA_HOME=$JAVA21HOME
 
 export SDKMAN_DIR=/usr/local/sdkman
 export PATH="${JAVA11HOME}/bin:${SDKMAN_DIR}/candidates/maven/current/bin:${PATH}"
