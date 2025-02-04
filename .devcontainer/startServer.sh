ed690ecd5fcd4b6168d7ed1693c15b2d6e6a4e45
AutoDev <autodev@microsoft.com>
2025-02-04 07:58:51 +0000

    Use JAVA11 as default JAVA_HOME

--- .devcontainer/startServer.sh
+++ .devcontainer/startServer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/local/sdkman/candidates/java/current
 export JAVA8HOME=/usr/local/sdkman/candidates/java/8.0.382-tem
 export JAVA11HOME=/usr/local/sdkman/candidates/java/11.0.20-tem
 export JAVA17HOME=/usr/local/sdkman/candidates/java/17.0.8-tem
 export JAVA21HOME=/usr/local/sdkman/candidates/java/21.0.4-tem
+export JAVA_HOME=$JAVA11HOME
 
 export SDKMAN_DIR=/usr/local/sdkman
 export PATH="${JAVA11HOME}/bin:${SDKMAN_DIR}/candidates/maven/current/bin:${PATH}"
