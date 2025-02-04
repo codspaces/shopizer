c1ad13622bcb4ded743a98f8ecad48ee3a754d91
Thomas Bombach <tbombach@users.noreply.github.com>
2025-02-03 21:28:07 -0800

    Update startServer.sh

--- .devcontainer/startServer.sh
+++ .devcontainer/startServer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-tar -xzf /workspace/microsoft-autodev-0.5.0.tgz -C /workspace/
-cd /workspace/package
-npm run start:server
+tar -xzf /workspace/microsoft-autodev-0.7.0.tgz -C /workspace/
+node /workspace/package/dist/src/java-upgrade/webServer/server.js
 
 exit 0
